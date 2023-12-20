e5fe1960446dd76ab91b6f67acccdee1da6e2684
Arkimedij <121115757+Arkimedij@users.noreply.github.com>
2023-12-20 07:01:58 +0545

    first commit

--- todolist/todoapp/views.py
+++ todolist/todoapp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from . models import Task
 # from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    context = {'success' : False}
+    if request.method == "POST":
+        title = request.POST['title']
+        desc = request.POST['desc']
+        print(title,desc)
+        details = Task(taskTitle=title,taskdesc=desc)
+        details.save()
+        context = {'success' : True}
+    return render(request,'home.html',context)
 
 def task(request):
     return render(request,'task.html')
